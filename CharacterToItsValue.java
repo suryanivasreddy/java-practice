d218378e29224e64e171ec2468c5b86749505229
Surya Nivas Reddy <suryanivasreddy@gmail.com>
2025-10-20 15:21:10 +0530

    java-practice-day-6

---
+++ CharacterToItsValue.java
@@ -0,0 +1,15 @@
+package com.codegnan.operatorexamples;
+
+import java.util.Scanner;
+
+public class CharacterToItsValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+int l=s.next().charAt(0);
+System.out.println(l);
+s.close();
+	}
+
+}
